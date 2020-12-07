8b2188c5219c86245d5c9d05876d906aae087d1b
unknown <2173656118@qq.com>
2020-12-07 14:05:03 +0800

    加入了一个tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <MainTabBar></MainTabBar>
   </div>
 </template>
 
+<script>
+import MainTabBar from 'components/content/maintabbar/MainTabBar'
+  export default {
+    name:'App',
+    components:{
+      MainTabBar
+    }
+  }
+</script>
 <style>
+  @import "assets/css/base.css";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,32 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const Home = () => import('../views/home/Home')
+const Cart = () => import('../views/cart/Cart')
+const Category = () => import('../views/category/Category')
+const Profile = () => import('../views/profile/Profile')
+
 const routes = [
+  {
+    path: '',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/cart',
+    component:Cart
+  },
+  {
+    path:'/category',
+    component:Category
+  },
+  {
+    path:'/profile',
+    component:Profile
+  },
 ]
 
 const router = new VueRouter({
