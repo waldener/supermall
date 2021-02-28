a645876759df2260e3eb3078ac423050fbf5e900
waldener <2173656118@qq.com>
2021-02-28 16:51:42 +0800

    push site

--- src/common/const.js
+++
@@ -1,4 +0,0 @@
-export const TOP_DISTANCE = 1000
-export const POP = 'pop'
-export const NEW = 'new'
-export const SELL = 'sell'
--- src/store/mutation-types.js
+++
@@ -1,2 +0,0 @@
-export const ADD_TO_CART = 'add_to_cart'
-export const INCREMENT_COUNT = 'increment_count'
