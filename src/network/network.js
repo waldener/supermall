1496cc1a27af525fcb38fd127d19516989f9dcd0
waldener <2173656118@qq.com>
2021-04-12 16:33:33 +0800

    更新接口

--- src/network/network.js
+++ src/network/network.js
@@ -24,24 +24,3 @@ export function request(options) {
   })
 }
 
-// ES5封装方式
-// export function request(options, success, failure) {
-//   // 1.创建axios的实例对象
-//   const instance = axios.create({
-//     baseURL: 'http://123.207.32.32:8000',
-//     timeout: 5000
-//   })
-//
-//   // 过滤器(拦截器)
-//   instance.interceptors.response.use(res => {
-//     return res.data
-//   })
-//
-//   // 通过实例发送网络请求
-//   instance(options)
-//       .then(res => {
-//         success(res)
-//       }).catch(err => {
-//         failure(err)
-//       })
-// }
